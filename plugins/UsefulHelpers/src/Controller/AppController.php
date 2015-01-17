b908012fa43160a7f74f8015f97c43ca2b4607aa
Otsuki Hitoshi <hitochan777@gmail.com>
2015-01-18 00:00:15 +0900

    delete plugins from under control

--- plugins/UsefulHelpers/src/Controller/AppController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UsefulHelpers\Controller;
-
-use App\Controller\AppController as BaseController;
-
-class AppController extends BaseController {
-
-}
--- plugins/UsefulHelpers/tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Test suite bootstrap for UsefulHelpers.
- */
-// Customize this to be a relative path for embedded plugins.
-// For standalone plugins, this should point at a CakePHP installation.
-require '/var/www/kitweb/config/bootstrap.php';
