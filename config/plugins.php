6be4c61b57ff35db11f7f009f8fb55ed8a07370c
Otsuki Hitoshi <hitochan777@gmail.com>
2015-01-19 15:03:44 +0900

    plugins updated

--- config/plugins.php
+++ config/plugins.php
@@ -1,10 +1,12 @@
 <?php
 $baseDir = dirname(dirname(__FILE__));
-return array (
-  'plugins' => 
-  array (
-    'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
-    'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
-    'Bake' => $baseDir . '/vendor/cakephp/bake/',
-  ),
-);
+return [
+    'plugins' => [
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'CakePdf' => $baseDir . '/vendor/friendsofcake/cakepdf/',
+        'Bootstrap3' => $baseDir . '/vendor/holt59/cakephp3-bootstrap3-helpers/',
+
+    ]
+];
